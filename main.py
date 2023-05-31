1ef370732fe87f6399680b8929426a3742768a9c
mrestivill <mrodriguezestivill@gmail.com>
2023-05-31 13:23:14 +0200

    Added comments to developers

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+# Do not edit the signature of this function
+
 def run(input_data, solver_params, extra_arguments):
+    # this output will be converted to json file once executed.
+    output = {}
+    # This is the core of your algorithm, 
+    # here you must return the corresponding answer.
+    
+    # Do not catch exceptions, let the platform handle them for you. The trace error will be shown on the platform.
+    
+    
+    # print("use as many prints as possible during the development phase")
 
-    #This is the core of your algorithm, 
-    #here you must return the corresponding answer.
-
-    return solution
+    # The output of the algorithm.
+    return output
     
 
